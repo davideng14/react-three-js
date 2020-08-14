387f04c6a99a83aec12ce95d9faa5faf83d0e884
Jose Rojas <jose.rojas@prodigious.com>
2020-08-14 16:49:42 -0500

    build source

---
+++ build/precache-manifest.a0e3f7c44c70d47d986f3d7bf4d69cac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f51a764c731096e95ca3742d04892abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3992644f6bc051c9edf6",
+    "url": "/static/css/main.03dadb4b.chunk.css"
+  },
+  {
+    "revision": "29f9f4582b9805579657",
+    "url": "/static/js/2.a4b9bba8.chunk.js"
+  },
+  {
+    "revision": "3992644f6bc051c9edf6",
+    "url": "/static/js/main.ce45b875.chunk.js"
+  },
+  {
+    "revision": "80b789232f75104194ee",
+    "url": "/static/js/runtime-main.e2fb13b6.js"
+  }
+]);
